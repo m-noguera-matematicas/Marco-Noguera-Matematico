b9e377ddbe90357a3f76918050e8a3e9b2a18cac
m-noguera-matematicas <marconoguera@gmail.com>
2026-06-18 11:24:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -25,12 +25,11 @@ Soy **Marco Antonio Noguera Alvarenga**, venezolano de nacimiento y mexicano de 
 Desde hace más de **17 años** tengo el privilegio de dedicarme a la docencia universitaria,
 una profesión que considero no solo un trabajo, sino una verdadera vocación.
 
-Mi formación académica es la de **Matemático**. A lo largo de mi trayectoria he impartido
-cursos de cálculo, probabilidad, estadística, investigación de operaciones y diseño de
-experimentos, convencido de que las matemáticas son una herramienta fundamental para
-comprender el mundo y desarrollar el pensamiento crítico. Creo firmemente que la educación es el camino más sólido para construir una mejor sociedad,
-y que un buen profesor no solo transmite conocimientos, sino también curiosidad,
-disciplina y el deseo permanente de aprender.
+Mi formación académica es la de **Matemático**. A lo largo de mi trayectoria he impartido 
+diferentes tipos de cursos entorno a las matemáticas a nivel universitario, convencido 
+de que las matemáticas son una herramienta fundamental para
+comprender el mundo y desarrollar el pensamiento crítico y que la educación es el camino 
+más sólido para construir una mejor sociedad.
 
 Fuera del aula disfruto de una buena taza de café, la ciencia ficción, la lectura,
 el metal sinfónico y el estudio de temas relacionados con la ciencia y la filosofía.
